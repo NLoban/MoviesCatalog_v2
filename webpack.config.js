912cdc0192b4af8079bde1db3db5592b42291b40
Nadezhda Loban <nadine.loban@gmail.com>
2017-02-28 18:23:39 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var webpack = require("webpack");
+
+module.exports = {
+  entry: {
+    "vendor": "./app/vendor",
+    "app": "./app/main"
+  },
+  output: {
+    path: __dirname,
+    filename: "./dist/[name].bundle.js"
+  },
+  resolve: {
+    extensions: ['', '.ts', '.js']
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.ts/,
+        loaders: ['ts-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader?minimise=true'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file?name=assets/[name].[hash].[ext]'
+      },
+      {
+        test: /\.css$/,
+        loaders: ['to-string-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"./dist/vendor.bundle.js")
+  ]
+}
